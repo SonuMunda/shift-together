e9b20ec8275c962080ff65043deb9029fb480564
SonuMunda <sonumunda1312@gmail.com>
2023-04-01 16:46:52 +0530

    Registration Validation Added

--- client/src/Pages/Register/Register.jsx
+++ client/src/Pages/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Register.css";
 import { Link } from "react-router-dom";
 import { BiUser, BiAt, BiMobileAlt, BiLock } from "react-icons/bi";
@@ -6,10 +6,87 @@ import signupImg from "/images/signup_bg.png";
 import RentWidget from "../../components/RentWidget/RentWidget";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+    cpassword: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Check if all form fields are filled
+    if (
+      !formData.username ||
+      !formData.email ||
+      !formData.phone ||
+      !formData.password ||
+      !formData.cpassword
+    ) {
+      alert("Please fill all form fields.");
+      return;
+    }
+
+    // Number validation
+    const numberRegex = /\d+/;
+    if (numberRegex.test(formData.username)) {
+      alert("Username should not contain any numbers.");
+      return;
+    }
+
+    // Size validation
+    if (formData.username.length < 4 || formData.username.length > 20) {
+      alert("Username must between 4 and 20 characters");
+      return;
+    }
+
+    // Check if email is valid
+    const emailRegex = /\S+@\S+\.\S+/;
+    if (!emailRegex.test(formData.email)) {
+      alert("Please enter a valid email address.");
+      return;
+    }
+
+    // Check if phone number is valid
+    const phoneRegex = /^\d{10}$/;
+    if (!phoneRegex.test(formData.phone)) {
+      alert("Please enter a valid 10-digit phone number.");
+      return;
+    }
+
+    // Check if password and confirm password match
+    if (formData.password !== formData.cpassword) {
+      alert("Passwords do not match.");
+      return;
+    }
+
+    // All form fields are valid, submit the form
+    console.log(formData);
+
+    // Clear the form
+    setFormData({
+      username: "",
+      email: "",
+      phone: "",
+      password: "",
+      cpassword: "",
+    });
+  };
+
   return (
     <div className="main-wrapper">
-{/* Rent Widget */}
-      <RentWidget/>
+      {/* Rent Widget */}
+      <RentWidget />
       <section className="signup">
         <div className="signup-container">
           <div className="signup-row">
@@ -18,7 +95,7 @@ const Register = () => {
                 <div className="signup-heading">
                   <h4 className="signup-text p-2">Signup</h4>
                 </div>
-                <form className="signup-form">
+                <form className="signup-form" onSubmit={handleSubmit}>
                   <div className="form-group">
                     <div className="form-icon">
                       <BiUser />
@@ -28,6 +105,8 @@ const Register = () => {
                       name="username"
                       className="signup-form-control"
                       placeholder="Username"
+                      value={formData.username}
+                      onChange={handleChange}
                       required
                     />
                   </div>
@@ -40,6 +119,8 @@ const Register = () => {
                       name="email"
                       className="signup-form-control"
                       placeholder="Email"
+                      value={formData.email}
+                      onChange={handleChange}
                       required
                     />
                   </div>
@@ -52,6 +133,8 @@ const Register = () => {
                       name="phone"
                       className="signup-form-control"
                       placeholder="Phone"
+                      value={formData.phone}
+                      onChange={handleChange}
                       required
                     />
                   </div>
@@ -64,6 +147,8 @@ const Register = () => {
                       name="password"
                       className="signup-form-control"
                       placeholder="Password"
+                      value={formData.password}
+                      onChange={handleChange}
                       required
                     />
                   </div>
@@ -73,9 +158,11 @@ const Register = () => {
                     </div>
                     <input
                       type="password"
-                      name="conPassword"
+                      name="cpassword"
                       className="signup-form-control"
                       placeholder="Confirm Password"
+                      value={formData.cpassword}
+                      onChange={handleChange}
                       required
                     />
                   </div>
@@ -90,7 +177,7 @@ const Register = () => {
                 </div>
               </div>
             </div>
-
+            {/* Image */}
             <div className="signup-col" id="signup-img-col">
               <div className="signup-img">
                 <img src={signupImg} alt="image" />
