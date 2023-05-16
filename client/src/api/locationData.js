c02c562ad8ac7bd586e15bc2a566b9386d77fd04
SonuMunda <sonumunda1312@gmail.com>
2023-05-16 10:16:18 +0530

    final stage

--- client/src/api/locationData.js
+++ client/src/api/locationData.js
@@ -5,7 +5,7 @@ const locationData = [
         {
           srNo: 1,
           serviceName: "General Query",
-          helplineNumber: "0172-2222222",
+          helplineNumber: "0172-2222221",
         },
         {
           srNo: 2,
@@ -15,17 +15,17 @@ const locationData = [
         {
           srNo: 3,
           serviceName: "Electricity Management",
-          helplineNumber: "0172-2222222",
+          helplineNumber: "0172-2222223",
         },
         {
           srNo: 4,
           serviceName: "Medical Emergency",
-          helplineNumber: "0172-2222222",
+          helplineNumber: "0172-2222224",
         },
         {
           srNo: 5,
           serviceName: "Wifi Management",
-          helplineNumber: "0172-2222222",
+          helplineNumber: "0172-2222225",
         },
       ],
     },
@@ -35,12 +35,12 @@ const locationData = [
         {
           srNo: 1,
           serviceName: "General Query",
-          helplineNumber: "0172-3333333",
+          helplineNumber: "0172-3333331",
         },
         {
           srNo: 2,
           serviceName: "Water Management",
-          helplineNumber: "0172-3333333",
+          helplineNumber: "0172-3333332",
         },
         {
           srNo: 3,
@@ -50,12 +50,12 @@ const locationData = [
         {
           srNo: 4,
           serviceName: "Medical Emergency",
-          helplineNumber: "0172-3333333",
+          helplineNumber: "0172-3333334",
         },
         {
           srNo: 5,
           serviceName: "Wifi Management",
-          helplineNumber: "0172-3333333",
+          helplineNumber: "0172-3333335",
         },
       ],
     },
@@ -65,17 +65,17 @@ const locationData = [
         {
           srNo: 1,
           serviceName: "General Query",
-          helplineNumber: "0172-4444444",
+          helplineNumber: "0172-4444441",
         },
         {
           srNo: 2,
           serviceName: "Water Management",
-          helplineNumber: "0172-4444444",
+          helplineNumber: "0172-4444442",
         },
         {
           srNo: 3,
           serviceName: "Electricity Management",
-          helplineNumber: "0172-4444444",
+          helplineNumber: "0172-4444443",
         },
         {
           srNo: 4,
@@ -85,7 +85,7 @@ const locationData = [
         {
           srNo: 5,
           serviceName: "Wifi Management",
-          helplineNumber: "0172-4444444",
+          helplineNumber: "0172-4444445",
         },
       ],
     },
@@ -96,22 +96,22 @@ const locationData = [
         {
           srNo: 1,
           serviceName: "General Query",
-          helplineNumber: "0172-5555555",
+          helplineNumber: "0172-5555551",
         },
         {
           srNo: 2,
           serviceName: "Water Management",
-          helplineNumber: "0172-5555555",
+          helplineNumber: "0172-5555552",
         },
         {
           srNo: 3,
           serviceName: "Electricity Management",
-          helplineNumber: "0172-5555555",
+          helplineNumber: "0172-5555553",
         },
         {
           srNo: 4,
           serviceName: "Medical Emergency",
-          helplineNumber: "0172-5555555",
+          helplineNumber: "0172-5555554",
         },
         {
           srNo: 5,
@@ -127,28 +127,28 @@ const locationData = [
         {
           srNo: 1,
           serviceName: "General Query",
-          helplineNumber: "0172-6666666",
+          helplineNumber: "0172-6666661",
         },
         {
           srNo: 2,
           serviceName: "Water Management",
-          helplineNumber: "0172-6666666",
+          helplineNumber: "0172-6666662",
         },
         {
           srNo: 3,
           serviceName: "Electricity Management",
-          helplineNumber: "0172-6666666",
+          helplineNumber: "0172-6666663",
         },
         {
           srNo: 4,
 
           serviceName: "Medical Emergency",
-          helplineNumber: "0172-6666666",
+          helplineNumber: "0172-6666664",
         },
         {
           srNo: 5,
           serviceName: "Wifi Management",
-          helplineNumber: "0172-6666666",
+          helplineNumber: "0172-6666665",
         },
       ],
     },
@@ -159,27 +159,27 @@ const locationData = [
         {
           srNo: 1,
           serviceName: "General Query",
-          helplineNumber: "0172-7777777",
+          helplineNumber: "0172-7777771",
         },
         {
           srNo: 2,
           serviceName: "Water Management",
-          helplineNumber: "0172-7777777",
+          helplineNumber: "0172-7777772",
         },
         {
           srNo: 3,
           serviceName: "Electricity Management",
-          helplineNumber: "0172-7777777",
+          helplineNumber: "0172-7777773",
         },
         {
           srNo: 4,
           serviceName: "Medical Emergency",
-          helplineNumber: "0172-7777777",
+          helplineNumber: "0172-7777774",
         },
         {
           srNo: 5,
           serviceName: "Wifi Management",
-          helplineNumber: "0172-7777777",
+          helplineNumber: "0172-7777775",
         },
       ],
     },
